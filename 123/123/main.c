8198e1f955dfc416b831733d3255be5ad8849fd6
lk <719283438@qq.com>
2016-01-08 16:16:26 +0800

    abcd

---
+++ 123/123/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  123
+//
+//  Created by lk on 16/1/8.
+//  Copyright © 2016年 lk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("abc");
+    return 0;
+}
